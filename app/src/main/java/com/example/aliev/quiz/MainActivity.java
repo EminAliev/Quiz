5ac3113f92e5747bbc1c2582e1ae459fc804f3d6
aliev <sambufer200@gmail.com>
2019-04-12 22:08:54 +0300

    fixed mistake with database

--- app/src/main/java/com/example/aliev/quiz/MainActivity.java
+++ app/src/main/java/com/example/aliev/quiz/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.app.ActionBar;
 
 import com.example.aliev.quiz.datebases.DataBase;
 import com.example.aliev.quiz.decoration.Decorator;
@@ -21,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("QUIZ");
         setSupportActionBar(toolbar);
@@ -28,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
         recyclerView = (RecyclerView) findViewById(R.id.recycle_category);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
-
 
 
         Adapter adapter = new Adapter(MainActivity.this, DataBase.getInst(this).getCategoriesFromDataBase());
---
+++ app/src/main/java/com/example/aliev/quiz/decoration/Common.java
@@ -0,0 +1,8 @@
+package com.example.aliev.quiz.decoration;
+
+import com.example.aliev.quiz.model.Category;
+
+public class Common {
+
+    public static Category select = new Category();
+}
